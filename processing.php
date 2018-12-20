286748f3f93cbaf65c7ccc46bf498260010afd64
todd <toddtran9@gmail.com>
2018-12-20 15:08:11 -0800

    Instantiate an object to hold the data entered from our upload form.

--- processing.php
+++ processing.php
@@ -8,6 +8,13 @@ if(!isset($_POST['uploadButton'])) {
 }
 
 // 1) create file upload data.
+$videoUploadData = new VideoUploadData($_POST["fileInput"],
+                                        $_POST["titleInput"],
+                                        $_POST["descriptionInput"],
+                                        $_POST["privacyInput"],
+                                        $_POST["categoryInput"],
+                            "REPLACE-THIS"
+                                        );
 // 2) Process video data (upload).
 // 3) Check if upload was successful.
 ?>
