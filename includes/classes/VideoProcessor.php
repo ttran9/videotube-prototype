b1a45bd266c7c410638bcd923248bf2e196434eb
todd <toddtran9@gmail.com>
2018-12-20 15:59:18 -0800

    Creating logic to generate a new file name (unique name).

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -8,7 +8,12 @@ class VideoProcessor {
     }
 
     public function upload($videoUploadData) {
-
+        $targetDir = "uploads/videos/";
+        $videoData = $videoUploadData->getVideoDataArray();
+        $tempFilePath = $targetDir . uniqid() . basename($videoData['name']);
+//        $tempFilePath = str_replace("\\s+", "_", $tempFilePath);
+        $tempFilePath = str_replace(" ", "_", $tempFilePath);
+        echo $tempFilePath;
     }
 }
 ?>
