afbce4b76c5e848f9436a9c0c2782bc7545b9e67
todd <toddtran9@gmail.com>
2018-12-23 13:47:44 -0800

    Creating a class to be able to create our video.

---
+++ includes/classes/Video.php
@@ -0,0 +1,50 @@
+<?php
+class Video {
+
+    private $con, $sqlData, $userLoggedInObj;
+
+    public function __construct($con, $input, $userLoggedInObj) {
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+
+        if(is_array($input)) {
+            $this->sqlData = $input;
+        } else {
+            $query = $this->con->prepare("SELECT * FROM video WHERE id = :id");
+            $query->bindParam(":id", $input);
+            $query->execute();
+
+            $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+        }
+
+    }
+
+    public function getUsername() {
+        return $this->sqlData["username"];
+    }
+
+    public function getName() {
+        return $this->sqlData["firstName"] . " " . $this->sqlData["lastName"];
+    }
+
+    public function getFirstName() {
+        return $this->sqlData["firstName"];
+    }
+
+    public function getLastName() {
+        return $this->sqlData["lastName"];
+    }
+
+    public function getEmail() {
+        return $this->sqlData["email"];
+    }
+
+    public function getProfilePic() {
+        return $this->sqlData["profilePic"];
+    }
+
+    public function getSignupDate() {
+        return $this->sqlData["signUpDate"];
+    }
+}
+?>
