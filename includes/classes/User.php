5d563de31cf53338b17886722d3c55b22742d346
todd <toddtran9@gmail.com>
2018-12-23 13:33:13 -0800

    Creating functions to retrieve information about the user such as first name, last name, email, and other fields.

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -2,9 +2,43 @@
 class User {
 
     private $con, $sqlData;
-    public function __construct($con, $sqlData) {
+
+    public function __construct($con, $username) {
         $this->con = $con;
-        $this->sqlData = $sqlData;
+
+        $query = $this->con->prepare("SELECT * FROM users WHERE username = :username");
+        $query->bindParam(":username", $username);
+        $query->execute();
+
+        $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getUsername() {
+        return $this->sqlData["username"];
+    }
+
+    public function getName() {
+        return $this->sqlData["firstName"] . " " . $this->sqlData["lastName"];
+    }
+
+    public function getFirstName() {
+        return $this->sqlData["firstName"];
+    }
+
+    public function getLastName() {
+        return $this->sqlData["lastName"];
+    }
+
+    public function getEmail() {
+        return $this->sqlData["email"];
+    }
+
+    public function getProfilePic() {
+        return $this->sqlData["profilePic"];
+    }
+
+    public function getSignupDate() {
+        return $this->sqlData["signUpDate"];
     }
 }
 ?>
