150ae0147db556147f2810723740ac816d676d4d
todd <toddtran9@gmail.com>
2018-12-23 01:22:51 -0800

    Creating constants for the errors related to the email field.

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -3,6 +3,9 @@ class Constants {
     public static $firstNameCharacters = "Your first name must be between 2 and 25 characters.";
     public static $lastNameCharacters = "Your last name must be between 2 and 25 characters.";
     public static $usernameCharacters = "Your user name must be between 5 and 25 characters.";
-    public static $usernameTaken = "This username already exists";
+    public static $usernameTaken = "This username already exists.";
+    public static $emailsDoNotMatch = "Your emails do not match.";
+    public static $emailInvalid = "Please enter a valid email address.";
+    public static $emailTaken = "This email is already in use.";
 }
 ?>
