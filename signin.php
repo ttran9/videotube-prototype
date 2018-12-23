7de71e0d866fad179a5131971075307a7587a362
todd <toddtran9@gmail.com>
2018-12-22 22:34:49 -0800

    Creating the template/page to present the signin form.

---
+++ signin.php
@@ -0,0 +1,34 @@
+<?php require_once("includes/config.php"); ?>
+
+<!doctype html>
+<html>
+<head>
+    <title>VideoTube</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <div class="signinContainer">
+        <div class="column">
+            <div class="header">
+
+            </div>
+
+            <div class="loginForm">
+
+            </div>
+
+            <a href="signin.php" class="signInMessage">Already have an account? Sign In Here!</a>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
