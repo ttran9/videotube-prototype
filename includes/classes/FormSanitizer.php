f546e73eed7a073a930d192e5eae12c43a8f81d7
todd <toddtran9@gmail.com>
2018-12-23 00:19:37 -0800

    Adding in other sanitizing functions for the username, email, and password fields.

--- includes/classes/FormSanitizer.php
+++ includes/classes/FormSanitizer.php
@@ -10,5 +10,23 @@ class FormSanitizer {
         return $inputText;
     }
 
+    public static function sanitizeFormUsername($inputText) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
+
+    public static function sanitizeFormPassword($inputText) {
+        $inputText = strip_tags($inputText);
+        return $inputText;
+    }
+
+    public static function sanitizeFormEmail($inputText) {
+        // separated from the username because we may add functionality later on.
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
+
 }
 ?>
