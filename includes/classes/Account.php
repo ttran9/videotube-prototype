580ee7514913f965a625c7ad3336128a2b03f2cc
todd <toddtran9@gmail.com>
2018-12-23 00:39:20 -0800

    Create a function to validate the length of the first name entered.

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -2,13 +2,20 @@
 class Account {
 
     private $con;
+    private $errorArray = array();
 
     public function __construct($con) {
         $this->con = $con;
     }
 
     public function register($firstName, $lastName, $userName, $email, $email2, $password, $password2) {
+        $this->validFirstName($firstName);
+    }
 
+    private function validFirstName($firstName) {
+        if(strlen($firstName) < 2 || strlen($firstName) > 25) {
+            array_push($this->errorArray, Constants::$firstNameCharacters);
+        }
     }
 
 }
