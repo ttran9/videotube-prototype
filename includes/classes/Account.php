a23e4bd1dca5db287dc65ff5a22e12a2ab23a201
todd <toddtran9@gmail.com>
2018-12-23 00:59:37 -0800

    Creating a function to validate the last name.

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -10,11 +10,19 @@ class Account {
 
     public function register($firstName, $lastName, $userName, $email, $email2, $password, $password2) {
         $this->validFirstName($firstName);
+        $this->validLastName($lastName);
     }
 
     private function validFirstName($firstName) {
         if(strlen($firstName) < 2 || strlen($firstName) > 25) {
             array_push($this->errorArray, Constants::$firstNameCharacters);
+        }
+    }
+
+    private function validLastName($lastName) {
+        // can do other validations if necessary....
+        if(strlen($lastName) < 2 || strlen($lastName) > 25) {
+            array_push($this->errorArray, Constants::$lastNameCharacters);
         }
     }
 
