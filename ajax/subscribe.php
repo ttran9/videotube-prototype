6ed730cfaa0f8fa45750df212fbcb58a3ce51e75
todd <toddtran9@gmail.com>
2018-12-24 01:36:29 -0800

    Checking if the values are passed in properly in order to create a subscription.

--- ajax/subscribe.php
+++ ajax/subscribe.php
@@ -1,2 +1,9 @@
 <?php
+require_once("../includes/config.php");
+if(isset($_POST['userTo']) && isset($_POST['userFrom'])) {
+    echo "good!!";
+}
+else {
+    echo "One or more parametsr are not passed into the subscribe.php file.";
+}
 ?>
