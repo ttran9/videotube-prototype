25c60b98f6c4e00bcb40c559f97dc1a4fc7d2410
todd <toddtran9@gmail.com>
2018-12-24 01:51:00 -0800

    Updating the subscription status and the text without requiring a page refresh.

--- assets/jss/userActions.js
+++ assets/jss/userActions.js
@@ -4,7 +4,14 @@ function subscribe(userTo, userFrom, button) {
         return ;
     }
     $.post("ajax/subscribe.php", {userTo: userTo, userFrom: userFrom})
-    .done(function(data) {
-        console.log(data);
+    .done(function(count) {
+        if(count != null) {
+            $(button).toggleClass("subscribe unsubscribe"); // adds the other class and removes the other class (inverts the classes).
+            var buttonText = $(button).hasClass("subscribe") ? "SUBSCRIBE" : "SUBSCRIBED";
+            $(button).text(buttonText + " " + count);
+        } else {
+            // null would mean there is no data returned. unlikely to be happen.....
+            alert("something went wrong");
+        }
     });
 }
