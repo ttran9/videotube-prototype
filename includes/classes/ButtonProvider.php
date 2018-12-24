f6ea370a23002a1690f3fa913b1e7c6cd6dcb19f
todd <toddtran9@gmail.com>
2018-12-24 00:38:40 -0800

    Creating a function to create the button which allows us to edit the video.

--- includes/classes/ButtonProvider.php
+++ includes/classes/ButtonProvider.php
@@ -19,6 +19,17 @@ class ButtonProvider {
                 </button>";
     }
 
+    public static function createHyperlinkButton($text, $imageSrc, $href, $class) {
+        $image = $imageSrc == null ? "" : "<img src='$imageSrc'/>";
+
+        return "<a href='$href'>
+                    <button class='$class'>
+                        $image
+                        <span class='text'>$text</span>
+                    </button>
+                </a>";
+    }
+
     public static function createUserProfileButton($con, $username) {
         $userObj = new User($con, $username);
         $profilePic = $userObj->getProfilePic();
@@ -27,6 +38,16 @@ class ButtonProvider {
         return "<a href='$link'>
                     <img src='$profilePic' class='profilePicture'/>
                 </a>";
+    }
+
+    public static function createEditVideoButton($videoId) {
+        $href = "editVideo.php?videoId=$videoId";
+
+        $button = ButtonProvider::createHyperlinkButton("Edit Video", null, $href, "edit button");
+
+        return "<div class='editVideoButtonContainer'>
+                    $button
+                </div>";
     }
 }
 
