feed664d8c4c3cfa8bad5a949c7f14a4871af82c
todd <toddtran9@gmail.com>
2018-12-23 22:22:09 -0800

    Adding the active class to the "like" button and removing the "active" class from the dislike button if it is present.

--- assets/jss/videoPlayerActions.js
+++ assets/jss/videoPlayerActions.js
@@ -1,6 +1,11 @@
 function likeVideo(button, videoId) {
     $.post("ajax/likeVideo.php", {videoId: videoId})
     .done(function(data) {
-        alert(data);
+
+        var likeButton = $(button); // create a jquery object from the javascript button (to be able to use jquery functions).
+        var dislikeButton = $(button).siblings(".dislikeButton");
+
+        likeButton.addClass("active");
+        dislikeButton.removeClass("active");
     });
 }
