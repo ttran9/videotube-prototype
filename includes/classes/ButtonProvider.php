c998ffd4c2298be722f23537a26a70040b065b81
todd <toddtran9@gmail.com>
2018-12-23 23:49:13 -0800

    Creating the action link if the user is logged in and if not then call a JavaScript function alert the user.

--- includes/classes/ButtonProvider.php
+++ includes/classes/ButtonProvider.php
@@ -1,9 +1,17 @@
 <?php
 class ButtonProvider {
+
+    public static $signInFunctino = "notSignedIn()";
+
+    public static function createLink($link) {
+        return User::isLoggedIn() ? $link : ButtonProvider::$signInFunctino;
+    }
+
     public static function createButton($text, $imageSrc, $action, $class) {
         $image = $imageSrc == null ? "" : "<img src='$imageSrc'/>";
 
         // change action if needed
+        $action = ButtonProvider::createLink($action);
 
         return "<button class='$class' onclick='$action'>
                     $image
