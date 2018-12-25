5db420632cc3af0439bca8522b4a9a4fe8f62a1b
todd <toddtran9@gmail.com>
2018-12-24 17:38:10 -0800

    Getting information from the comment form and inserting the comment into the database.

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -1,7 +1,21 @@
 <?php
 require_once("../includes/config.php");
 if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
-    echo "success";
+    $commentText = $_POST['commentText'];
+    $postedBy = $_POST['postedBy'];
+    $videoId = $_POST['videoId'];
+    $responseTo = $_POST['responseTo'];
+
+    $query = $con->prepare("INSERT INTO comments(postedBy, videoId, responseTo, body)
+                                       VALUES(:postedBy, :videoId, :responseTo, :body)");
+    $query->bindParam(":postedBy", $postedBy);
+    $query->bindParam(":videoId", $videoId);
+    $query->bindParam(":responseTo", $responseTo);
+    $query->bindParam(":body", $commentText);
+
+    $query->execute();
+
+    // return new comment html.
 }
 else {
     echo "One or more parameters are not passed into the subscribe.php file.";
