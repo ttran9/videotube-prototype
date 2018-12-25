8a73eecabd26024431ee557cddca4e128cd2987c
todd <toddtran9@gmail.com>
2018-12-24 21:34:06 -0800

    Setting up function to make an AJAX call to like a comment.

--- assets/js/commentActions.js
+++ assets/js/commentActions.js
@@ -20,3 +20,31 @@ function toggleReply(button) {
 
     commentForm.toggleClass("hidden");
 }
+
+function likeComment(commentId, button, videoId) {
+    $.post("ajax/likeComment.php", {videoId: videoId, commentId: commentId})
+        .done(function(data) {
+
+            var likeButton = $(button); // create a jquery object from the javascript button (to be able to use jquery functions).
+            var dislikeButton = $(button).siblings(".dislikeButton");
+
+            likeButton.addClass("active");
+            dislikeButton.removeClass("active");
+
+            var result = JSON.parse(data);
+            updateLikesValue(likeButton.find(".text"), result.likes);
+            updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+            if(result.likes < 0) {
+                likeButton.removeClass("active");
+                likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png"); // find the first image and stop.
+            } else {
+                likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up-active.png");
+            }
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+        });
+}
+
+function dislikeComment(commentId, button, videoId) {
+
+}
