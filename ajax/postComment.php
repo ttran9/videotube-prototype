c9d717573ba9e660e07c8411d204f1c9fcc1ef01
todd <toddtran9@gmail.com>
2018-12-24 17:49:09 -0800

    Creating the new comment to be displayed (implementation later).

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -1,5 +1,7 @@
 <?php
 require_once("../includes/config.php");
+require_once("../includes/classes/User.php");
+require_once("../includes/classes/Comment.php");
 if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
     $commentText = $_POST['commentText'];
     $postedBy = $_POST['postedBy'];
@@ -15,7 +17,9 @@ if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['vi
 
     $query->execute();
 
-    // return new comment html.
+    $userLoggedInObj = new User($con, $_SESSION['userLoggedIn']);
+    $newComment = new Comment($con, $con->lastInsertId(), $userLoggedInObj, $videoId);
+    echo $newComment->create();
 }
 else {
     echo "One or more parameters are not passed into the subscribe.php file.";
