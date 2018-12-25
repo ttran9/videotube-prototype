bc22a1d180c36896679a68bf2eac6955aedebdf5
todd <toddtran9@gmail.com>
2018-12-24 17:49:28 -0800

    Creating the comment object.

---
+++ includes/classes/Comment.php
@@ -0,0 +1,26 @@
+<?php
+class Comment {
+
+    private $con, $sqlData, $userLoggedInObj, $videoId;
+
+    public function __construct($con, $input, $userLoggedInObj, $videoId) {
+
+        if(!is_array($input)) {
+            $query = $con->prepare("SELECT * FROM comments where id=:id");
+            $query->bindParam(":id", $input);
+            $query->execute();
+
+            $input = $query->fetch(PDO::FETCH_ASSOC);
+        }
+
+        $this->con = $con;
+        $this->videoId = $videoId;
+        $this->sqlData = $input;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+
+    public function create() {
+
+    }
+}
+?>
