d2df3b774eee85733ca7b523dd419852f88f8806
todd <toddtran9@gmail.com>
2018-12-24 21:34:50 -0800

    Handling the AJAX POST request to dislike a comment.

---
+++ ajax/dislikeComment.php
@@ -0,0 +1,13 @@
+<?php
+require_once("../includes/config.php");
+require_once("../includes/classes/Comment.php");
+require_once("../includes/classes/User.php");
+
+$username = $_SESSION['userLoggedIn'];
+$videoId = $_POST['videoId'];
+$commentId = $_POST['id'];
+$userLoggedInObj = new User($con, $username);
+$comment = new Comment($con, $commentId, $videoId, $userLoggedInObj);
+
+echo $comment->dislike();
+?>
