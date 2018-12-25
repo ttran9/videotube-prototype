fcb3d04522d6e2fcfdfcedfb532b8fad3a5564a1
todd <toddtran9@gmail.com>
2018-12-24 23:07:22 -0800

    Adding in logic to determine if the comment will be put in the front (on top of a comment) or in response to a comment (nested and indented).

--- assets/js/commentActions.js
+++ assets/js/commentActions.js
@@ -7,7 +7,11 @@ function postComment(button, postedBy, videoId, replyTo, containerClass) {
         $.post("ajax/postComment.php", {commentText: commentText, postedBy: postedBy, videoId: videoId,
                 responseTo: replyTo})
         .done(function(comment) {
-            $("." + containerClass).prepend(comment);
+            if(!replyTo) {
+                $("." + containerClass).prepend(comment);
+            } else {
+                $(button).parent().siblings("." + containerClass).append(comment);
+            }
         });
     } else {
         alert("You can't post an empty comment.");
